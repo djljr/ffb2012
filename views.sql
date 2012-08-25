13d4be10f734b07caac14577d52dac4f5e76f411
Den Lipovsky <den@denlipovsky.com>
2012-08-25 17:43:28 -0400

    first pass at injuries

--- views.sql
+++ views.sql
@@ -1,5 +1,6 @@
 drop view consolidated;
 create or replace view consolidated as
+with players as (
 select 'QB' as position, name, team, overall, byeweek from quarterbacks
 union
 select 'KI', name, team, overall, byeweek from kickers
@@ -10,4 +11,6 @@ select 'TE', name, team, overall, byeweek from tightends
 union
 select 'WR', name, team, overall, byeweek from widereceivers
 union
-select 'DE', name, null, overall, byeweek from defenses;
+select 'DE', name, null, overall, byeweek from defenses)
+select p.*, i.status from players p
+ left join injuries i on p.name = i.name 
