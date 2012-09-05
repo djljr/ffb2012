89ad845ecfe09c0d5f4c3556c931a430643341ba
Den Lipovsky <den@denlipovsky.com>
2012-09-04 22:05:38 -0400

    adding sqlite3 db - probably don't need postgres for this and now i can version it on github

--- draft.sql
+++ draft.sql
@@ -5,7 +5,7 @@ create sequence draftpicks;
 create table draft ( 
     owner varchar(50), 
     player varchar(50), 
-    picknumber int4 default (nextval('draftpicks')),
-    picktime timestamp default current_timestamp,
+    picknumber int4 primary key autoincrement,
+    picktime timestamp default date('now'),
     constraint fk_draft_player_player_name foreign key (player) references player(name)
 );
